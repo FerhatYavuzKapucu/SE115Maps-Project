ed6cc152eb9558dc53db0ee90398d15adbe5529d
FerhatYavuzKapucu <152414421+FerhatYavuzKapucu@users.noreply.github.com>
2024-12-04 18:33:09 +0300

    Finishing the city class.
    I add basic setter and getter methods and also a toString method so we can print out the information about a specific city.

--- src/City.java
+++ src/City.java
@@ -6,4 +6,23 @@ public class City {
         this.cityName = cityName;
         this.cityIndex = cityIndex;
     }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+    public String getCityName() {
+        return cityName;
+    }
+
+    
+    public void setCityIndex(int cityIndex) {
+        this.cityIndex = cityIndex;
+    }
+    public int getCityIndex() {
+        return cityIndex;
+    }
+
+    public String toString() {
+        return "City{name = " + cityName + ". index = " + cityIndex + "}";
+    }
 }

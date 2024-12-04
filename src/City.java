d56f0fec64fd1abc9bdb0749460df04b4b37ff04
FerhatYavuzKapucu <152414421+FerhatYavuzKapucu@users.noreply.github.com>
2024-12-04 18:22:44 +0300

    I have created the city class in this commit.

---
+++ src/City.java
@@ -0,0 +1,9 @@
+public class City {
+    private String cityName;
+    private int cityIndex;  
+
+    public City(String cityName, int cityIndex) {
+        this.cityName = cityName;
+        this.cityIndex = cityIndex;
+    }
+}

c89af4a0c07a7fdad5431655f3ad1050677285da
FerhatYavuzKapucu <152414421+FerhatYavuzKapucu@users.noreply.github.com>
2024-12-15 16:56:12 +0300

    I have completed the constructor and started file reading part

--- src/CountryMap.java
+++ src/CountryMap.java
@@ -8,5 +8,31 @@ public class CountryMap {
     private int[][] routes;
     private int cityCount;
 
-    
+    public CountryMap(int maxCities) {
+        cities = new City[maxCities];
+        routes = new int[maxCities][maxCities];
+        cityCount = 0;
+    }
+
+    public void readFile(String fileName) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        String line;
+
+        line = reader.readLine();
+        int numCities = Integer.parseInt(line);
+        cityCount = numCities;
+
+        line = reader.readLine();
+        String[] cityNames = line.split(" ");
+        for(int i = 0; i < numCities; i++) {
+            cities[i] = new City(cityNames[i], i);
+        }
+
+
+
+    }
+
+
+
+
 }

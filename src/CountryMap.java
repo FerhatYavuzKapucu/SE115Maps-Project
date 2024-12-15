78efaa50ea5f2fc0f925ff9da5f24f8adeb75b06
FerhatYavuzKapucu <152414421+FerhatYavuzKapucu@users.noreply.github.com>
2024-12-15 17:30:27 +0300

    i have completed the file reader also addRoute method

--- src/CountryMap.java
+++ src/CountryMap.java
@@ -28,11 +28,39 @@ public class CountryMap {
             cities[i] = new City(cityNames[i], i);
         }
 
+        line = reader.readLine();
+        int numRoutes = Integer.parseInt(line);
 
+        for(int i = 0; i < numRoutes; i++) {
+            line = reader.readLine();
+            String[] routeInfo = line.split(" ");
+            String city1 = routeInfo[0];
+            String city2 = routeInfo[1];
+            int time = Integer.parseInt(routeInfo[2]);
+            
+           addRoute(city1, city2, time);
+            
+        }
 
+        System.out.println("File read is succesfull");
+                        
     }
 
+    public void addRoute(String city1, String city2, int time) {
+        int index1 = getCityIndex(city1);
+        int index2 = getCityIndex(city2);
 
+        routes[index1][index2] = time;
+        routes[index2][index1] = time;
+    }
 
-
+    private int getCityIndex(String name) {
+        for (int i = 0; i < cityCount; i++) {
+            if (cities[i].getCityName().equals(name)) {
+                return cities[i].getCityIndex();
+            }
+        }
+        
+        throw new IllegalArgumentException("City not found: " + name);
+    }
 }

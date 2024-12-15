76cd2d42533ed2292a32c53d7cf13d7d8d46a2eb
FerhatYavuzKapucu <152414421+FerhatYavuzKapucu@users.noreply.github.com>
2024-12-15 16:26:44 +0300

    I have started to implement the country map class

---
+++ src/CountryMap.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CountryMap {
+    
+    private City[] cities;
+    private int[][] routes;
+    private int cityCount;
+
+    
+}

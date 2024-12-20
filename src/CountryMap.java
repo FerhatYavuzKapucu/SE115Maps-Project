8255255804d3e997cbbcee4895f0931b9136f4f1
FerhatYavuzKapucu <152414421+FerhatYavuzKapucu@users.noreply.github.com>
2024-12-20 18:59:35 +0300

    Re-Writed countymap
    I realized that using buffered reader is banned so i updated the countrymap class

--- src/CountryMap.java
+++ src/CountryMap.java
@@ -1,6 +1,6 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class CountryMap {
     
@@ -15,44 +15,41 @@ public class CountryMap {
     }
 
     public void readFile(String fileName) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        Scanner scanner = new Scanner(new File(fileName)); 
         String line;
 
-        line = reader.readLine();
-        int numCities = Integer.parseInt(line);
+       
+        int numCities = Integer.parseInt(scanner.nextLine().trim());
         cityCount = numCities;
 
-        line = reader.readLine();
-        String[] cityNames = line.split(" ");
-        for(int i = 0; i < numCities; i++) {
+        
+        String[] cityNames = scanner.nextLine().trim().split(" ");
+        for (int i = 0; i < numCities; i++) {
             cities[i] = new City(cityNames[i], i);
         }
 
-        line = reader.readLine();
-        int numRoutes = Integer.parseInt(line);
+        int numRoutes = Integer.parseInt(scanner.nextLine().trim());
 
-        for(int i = 0; i < numRoutes; i++) {
-            line = reader.readLine();
+        
+        for (int i = 0; i < numRoutes; i++) {
+            line = scanner.nextLine().trim();
             String[] routeInfo = line.split(" ");
             String city1 = routeInfo[0];
             String city2 = routeInfo[1];
             int time = Integer.parseInt(routeInfo[2]);
+
             
-           addRoute(city1, city2, time);
-            
+            int index1 = getCityIndex(city1);
+            int index2 = getCityIndex(city2);
+
+            routes[index1][index2] = time;
+            routes[index2][index1] = time; 
         }
 
-        System.out.println("File read is succesfull");
-                        
+        scanner.close(); 
+        System.out.println("File read is successful!");
     }
 
-    public void addRoute(String city1, String city2, int time) {
-        int index1 = getCityIndex(city1);
-        int index2 = getCityIndex(city2);
-
-        routes[index1][index2] = time;
-        routes[index2][index1] = time;
-    }
 
     private int getCityIndex(String name) {
         for (int i = 0; i < cityCount; i++) {

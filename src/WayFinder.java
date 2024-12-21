7bc374ffdc5589bca090dae017224e35fb722237
FerhatYavuzKapucu <152414421+FerhatYavuzKapucu@users.noreply.github.com>
2024-12-21 13:52:02 +0300

    Basic methods for wayfinder

--- src/WayFinder.java
+++ src/WayFinder.java
@@ -1,3 +1,14 @@
 public class WayFinder {
-    
+    private CountryMap map;
+
+    public WayFinder(CountryMap map) {
+        this.map = map;
+    }
+
+    public void findPath(String firstCity, String lastClity) {
+        int[][] routes = map.getRoutes();
+        City[] cities = map.getCities();
+
+        
+    }
 }

2b2609020ad0749713ad08d7acf4105a2a5e038a
FerhatYavuzKapucu <152414421+FerhatYavuzKapucu@users.noreply.github.com>
2024-12-21 15:40:12 +0300

    Changing Main method again

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try {
-            
-            Scanner input = new Scanner(System.in);
-            System.out.print("Enter the input file name: ");
-            String fileName = input.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter full path of the input file: ");
+            String filePath = scanner.nextLine(); // Kullanıcıdan tam dosya yolunu al
 
-            
             CountryMap map = new CountryMap(10);
-            map.readFile(fileName);
+            map.readFile(filePath);
 
-            
             WayFinder wayFinder = new WayFinder(map);
             wayFinder.findPath(map.getFirstCity(), map.getLastCity());
 
